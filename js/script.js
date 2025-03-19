70f46f1d6f63eb38a224594aa75c6789022dcfe3
Lauraujensen <lauraujensen@hafnet.dk>
2025-03-19 12:30:15 +0100

    CSS rettet og JS tilføjet

--- js/script.js
+++ js/script.js
@@ -1,2 +1,8 @@
 'use strict';
 
+let sun = document.querySelector('.sun');
+let scene = document.querySelector('.scene');
+
+sun.addEventListener('click', () => { // Toggle gør, at man kan skifte mellem to tilstande. On/off eller nat/dag f.eks.
+    scene.classList.toggle('night');
+})
